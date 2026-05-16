0a71d6d34e77e6302b8bf365bc526af74232e748
AshtarSheran2333 <patrimusil@gmail.com>
2026-05-16 23:34:14 +0200

    cleanup INTERFACE_UI

--- Interface/INTERFACE_UI.f90
+++ Interface/INTERFACE_UI.f90
@@ -11,6 +11,7 @@ module INTERFACE_UI
     use iso_fortran_env
     use SWITCHES
     use UTILS_ERROR
+    use PRETTY_PRINT
     implicit none
     
 #include "utils_error_macros.h"
@@ -20,8 +21,7 @@ module INTERFACE_UI
                                         ui_filetype_xyz = 2,&
                                         ui_filetype_none = -1
 
-    logical, protected ::   ui_no_check = .false.,&
-                            ui_vmd_out  = .false.
+    logical, protected ::               ui_vmd_out  = .false.
 
     integer(kind=int8), protected ::    ui_filetype = ui_filetype_none
 
@@ -32,40 +32,34 @@ module INTERFACE_UI
     
     subroutine print_help()
         implicit none
-        character(len=*), parameter ::  f_line = "(A)",&
-                                        f_1tab = "(T4, A)",&
-                                        f_2tab = "(T8,A)"
 
         !TODO might have to be updated
-        write(output_unit,f_line)	""
-        write(output_unit,f_line)	"__________________________Help dialog of Interface_____________________________"
+        write(output_unit,f_line)	heading(flat_pattern, "Help dialog of Interface program")
         write(output_unit,f_line)	""                          
-        write(output_unit,f_line)	"_____________________________Mandatory options_________________________________"
+        write(output_unit,f_line)	heading(flat_pattern, "Mandatory options")
         write(output_unit,f_line)	""
         write(output_unit,f_line)	"-I(input) <file1> *<file2>"
-        write(output_unit,f_1tab)		"specifies the input files. Options are:"
+        write(output_unit,f_1tab)		"Specifies the input files. The options are:"
         write(output_unit,f_2tab)			"*.xyz (positions), *.xyz (velocities)"
         write(output_unit,f_2tab)			"*.gro (positions+velocities)"
         write(output_unit,f_2tab)			"*.trr, *.gro (one frame - can be only positions)"
         write(output_unit,f_line)	""
-        write(output_unit,f_line)	"___________________________Optional useful options_____________________________"
+        write(output_unit,f_line)	heading(flat_pattern, "Optional useful options")
         write(output_unit,f_line)	""
-        write(output_unit,f_line)   "-G(good)"
-        write(output_unit,f_1tab)       "skips checking of the input files"
-        write(output_unit,f_line)   ""
         write(output_unit,f_line)   "-H(help)"
-        write(output_unit,f_1tab)       "prints this help dialog"
+        write(output_unit,f_1tab)       "Prints this help dialog."
         write(output_unit,f_line)   ""
-        write(output_unit,f_line)   "__________________________Optional debugging options___________________________"
+        write(output_unit,f_line)   heading(flat_pattern, "Optional debugging options")
         write(output_unit,f_line)   ""
         write(output_unit,f_line)   "WARNING:"
         write(output_unit,f_1tab)       "These options will create HUGE ASCII files."
-        write(output_unit,f_1tab)       "These options are kept in the code mainly for historical reasons/debugging."
         write(output_unit,f_line)   ""
         write(output_unit,f_line)   "-V(vmdout)"
         write(output_unit,f_1tab)       "ASCII dump of interface points in .xyz format"
         write(output_unit,f_line)   ""
-        write(output_unit,f_line)   "for more details see documentation"
+        write(output_unit,f_line)   heading(wavy_pattern, "For more details see the documentation")
+        write(output_unit,f_line)   ""
+
     end subroutine print_help
     
     ! goes through program options and sets the program logic
@@ -75,6 +69,10 @@ module INTERFACE_UI
         character(len=256) :: op, arg, arg1
         character :: option
         integer(kind=int64) :: i
+
+        call print_art()
+        write(output_unit,f_line) heading(wavy_pattern,"Evaluation of the program options")
+        write(output_unit,f_line) ""
 
         if(command_argument_count() < 1) error_stop("The program needs to be called with some arguments, see help -h")
 
@@ -94,59 +92,51 @@ module INTERFACE_UI
 
             select case(option)
                 case('i')
+                    write(output_unit,f_line) "input file:"
+                    write(output_unit,f_line) ""
                     call get_switch_string(i, op, arg)
                     ! INPUT GRO
                     if(index(arg, '.gro') .ne. 0) then
-                        print*, "input file:"
-                        print*, ""
-                        print"(tr5,a,tr5,a)", ".gro multiple frame file: ", trim(arg)
-                        print*, ""
+                        write(output_unit, "(tr4,a,tr4,a)") ".gro multiple frame file: ", trim(arg)
+                        write(output_unit,f_line) ""
                         ui_filetype = ui_filetype_gro
                         ui_filename1 = trim(adjustl(arg))
                     ! INPUT XYZ
                     else if(index(arg, '.xyz') .ne. 0) then
                         call get_switch_string(i, op, arg1)
                         if(index(arg1, '.xyz') .ne. 0) then
-                            print*, "input files:"
-                            print*, ""
-                            print"(tr5,a,tr5,a)", ".xyz position file: ", trim(arg)
-                            print"(tr5,a,tr5,a)", ".xyz velocity file: ", trim(arg1)
-                            print*, ""
+                            write(output_unit,"(tr4,a,tr4,a)") ".xyz position file: ", trim(arg)
+                            write(output_unit,"(tr4,a,tr4,a)") ".xyz velocity file: ", trim(arg1)
+                            write(output_unit,f_line) ""
                             ui_filetype = ui_filetype_xyz
                             ui_filename1 = trim(adjustl(arg))
                             ui_filename2 = trim(adjustl(arg1))
                         else
                             !TODO just positions are sufficient
-                            error_stop("program needs position and velocity .xyz files in order position file velocity file")
+                            error_stop("program needs positions and velocities .xyz files")
                         end if
                     ! INPUT TRR
                     else if(index(arg, '.trr') .ne. 0) then
                         call get_switch_string(i, op, arg1)
                         if(index(arg1, '.gro') .ne. 0) then
-                            print*, "input files:"
-                            print*, ""
-                            print"(tr5,a,tr5,a)", ".trr trajectory file: ", trim(arg)
-                            print"(tr5,a,tr5,a)", ".gro atleast single frame file: ", trim(arg1)
-                            print*, ""
+                            write(output_unit,"(tr4,a,tr4,a)") ".trr trajectory file: ", trim(arg)
+                            write(output_unit,"(tr4,a,tr4,a)") ".gro atleast single frame file: ", trim(arg1)
+                            write(output_unit,f_line) ""
                             ui_filetype = ui_filetype_trr
                             ui_filename1 = trim(adjustl(arg))
                             ui_filename2 = trim(adjustl(arg1))
                         else
-                            error_stop("program needs .trr and .gro files in order .trr file .gro file")
+                            error_stop("program needs .trr and .gro file")
                         end if
                     else
                         error_stop("Unknown input file format: "//trim(arg))
                     end if
-
-                    i = i + 1
-                case('g')
-                    ui_no_check = .true.
                     i = i + 1
                 case('v')
-                    ui_no_check = .true.
+                    ui_vmd_out = .true.
                     i = i + 1
                 case default
-                    print*, "skipping invalid option ", trim(op)
+                    write(output_unit,f_line) "skipping invalid option: "//trim(op)
                     i = i + 1
             end select
 
@@ -154,12 +144,15 @@ module INTERFACE_UI
             if(i > command_argument_count()) exit
         end do
 
-        print*, ""
+        write(output_unit,f_line) ""
 
         ! check if all the mandatory options were selected
         if(ui_filetype == ui_filetype_none) then
             error_stop("input file(s) must be specified, see help -h")
         end if
+
+        write(output_unit,f_line) heading(flat_pattern,"Evaluation of the program options - DONE")
+        write(output_unit,f_line) ""
 
     end subroutine evaluate_program_options
 end module
