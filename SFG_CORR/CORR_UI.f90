09eee6230a4316cb3770900dd4e64c581dcc85e3
AshtarSheran2333 <patrimusil@gmail.com>
2026-05-20 14:10:52 +0200

    add todos, cleanup, SFG_CORR, SFG_CORR_UI

---
+++ SFG_CORR/CORR_UI.f90
@@ -0,0 +1,173 @@
+!-------------------------------------------------------------------------------
+!                               CORR_UI
+!
+!   This is a set of subroutines that handle program -> console interfacing
+!   of the SFG_CORR program
+!   
+!   e.g. handling program options, printing help, printing recap...
+!
+!-------------------------------------------------------------------------------
+module CORR_UI
+    use iso_fortran_env
+    use SWITCHES
+    use UTILS_ERROR
+    use PRETTY_PRINT
+    use FRAME_READERS
+    implicit none
+
+#include "utils_error_macros.h"
+
+    integer(kind=int8), parameter ::    ui_filetype_trr = 0,&
+                                        ui_filetype_gro = 1,&
+                                        ui_filetype_xyz = 2,&
+                                        ui_filetype_none = -1
+
+    integer(kind=int8), protected ::    ui_filetype = ui_filetype_none
+
+    character(len=128), protected ::    ui_filename1 = "",&
+                                        ui_filename2 = ""
+
+    contains
+    
+    subroutine print_help()
+        implicit none
+        !TODO NEEDS TO BE UPDATED
+        write(output_unit,f_line)   heading(flat_pattern,"Help dialog of SSP_CORR")
+        write(output_unit,f_line)   ""                          
+        write(output_unit,f_line)   heading(flat_pattern,"Mandatory options")
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-I(input) <file1> *<file2>"
+        write(output_unit,f_1tab)       "specifies the input files. Options are:"
+        write(output_unit,f_2tab)           "*.xyz (positions), *.xyz (velocities)"
+        write(output_unit,f_2tab)           "*.gro (positions+velocities)"
+        write(output_unit,f_2tab)           "*.trr, *.gro (one frame - can be only positions)"
+        write(output_unit,f_line)   "" !TODO this must be different, something like -g "NAME" -> mandatory -L, repeat
+        write(output_unit,f_line)   "-L(layer) <number1 (from)> <number2 (to)>"
+        write(output_unit,f_1tab)       "specifies the layers to be analyzed. Options are:"
+        write(output_unit,f_2tab)           "BOTTOM | 0 1 2 3 4 5 6 7 | 15 14 13 12 11 10 9 8 | TOP"
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   heading(flat_pattern,"Optional useful options")
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-O(output) <name>"
+        write(output_unit,f_1tab)       "<name> will be appended by the output file names"
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-S(self)" !ok, but only implemented option
+        write(output_unit,f_1tab)       "enables calculation of the cross correlation terms"
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-C(cross) <real number>" !ok, but not implemented for now
+        write(output_unit,f_1tab)       "enables calculation of the cross correlation terms"
+        write(output_unit,f_1tab)       "<real number> is the cutoff radius for the cross correlation terms in [A]" 
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-N(neighborlist)" !ok, but not implemented by now
+        write(output_unit,f_1tab)       "enables neighbor list for for the cross correlation calculation"
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-D(dipole_moment_and_polarizability_derivatives) <filename>" !get rid of it, just try to read a file, if not found, stick to defaults
+        write(output_unit,f_1tab)       "file with dipole moment and polarizability derivatives"
+        write(output_unit,f_1tab)       "file format is described in the documentation"
+        write(output_unit,f_1tab)       "default values are described in the documentation"
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   "-H(help)"
+        write(output_unit,f_1tab)       "prints this help dialog"
+        write(output_unit,f_line)   ""
+        write(output_unit,f_line)   heading(wavy_pattern,"For more details see documentation")
+        write(output_unit,f_line)   ""
+
+    end subroutine print_help
+
+    ! goes through program options and sets the program logic
+    !TODO needs to be updated and decided how it should work
+    subroutine evaluate_program_options(fr)
+        implicit none
+        class(frame_reader), allocatable, intent(inout) :: fr
+        character(len=256) :: op, arg, arg1
+        character :: option
+        integer(kind=int64) :: i
+
+        call print_art()
+        write(output_unit,f_line) heading(wavy_pattern,"Evaluation of the program options")
+        write(output_unit,f_line) ""
+
+        if(command_argument_count() < 1) error_stop("The program needs to be called with some arguments, see help -h")
+
+        ! is help switch present?
+        do i = 1, command_argument_count()
+            call get_command_argument(i, op)
+            if(get_flag(trim(op)) == 'h') then
+                call print_help()
+                stop
+            end if
+        end do
+
+        i = 1
+        do
+            call get_command_argument(i, op)
+            option = get_flag(trim(op))
+
+            select case(option)
+                case('i')
+                    write(output_unit,f_line) "-I(input):"
+                    call get_switch_string(i, op, arg)
+                    ! INPUT GRO
+                    if(index(arg, '.gro') .ne. 0) then
+                        write(output_unit, f_1tab) ".gro multiple frame file: "//trim(arg)
+                        write(output_unit,f_line) ""
+                        ui_filetype = ui_filetype_gro
+                        ui_filename1 = trim(adjustl(arg))
+                        allocate(gro_frame_reader :: fr)
+                        error_io_check(fr%open_file(ui_filename1), "unable to open "//ui_filename1)
+                    ! INPUT XYZ
+                    else if(index(arg, '.xyz') .ne. 0) then
+                        call get_switch_string(i, op, arg1)
+                        if(index(arg1, '.xyz') .ne. 0) then
+                            write(output_unit,f_1tab) ".xyz position file: "//trim(arg)
+                            write(output_unit,f_1tab) ".xyz velocity file: "//trim(arg1)
+                            write(output_unit,f_line) ""
+                            ui_filetype = ui_filetype_xyz
+                            ui_filename1 = trim(adjustl(arg))
+                            ui_filename2 = trim(adjustl(arg1))
+                            allocate(xyz_frame_reader :: fr)
+                            error_io_check(fr%open_file(ui_filename1, ui_filename2), "unable to open files "//ui_filename1//" "//ui_filename2)
+                        else
+                            !TODO just positions are sufficient
+                            error_stop("program needs positions and velocities .xyz files")
+                        end if
+                    ! INPUT TRR
+                    else if(index(arg, '.trr') .ne. 0) then
+                        call get_switch_string(i, op, arg1)
+                        if(index(arg1, '.gro') .ne. 0) then
+                            write(output_unit,f_1tab) ".trr trajectory file: "//trim(arg)
+                            write(output_unit,f_1tab) ".gro atleast single frame file: "//trim(arg1)
+                            write(output_unit,f_line) ""
+                            ui_filetype = ui_filetype_trr
+                            ui_filename1 = trim(adjustl(arg))
+                            ui_filename2 = trim(adjustl(arg1))
+                            allocate(trr_frame_reader :: fr)
+                            error_io_check(fr%open_file(ui_filename1, ui_filename2), "unable to open files "//ui_filename1//" "//ui_filename2)
+                        else
+                            error_stop("program needs .trr and .gro file")
+                        end if
+                    else
+                        error_stop("Unknown input file format: "//trim(arg))
+                    end if
+                    i = i + 1
+                case default
+                    write(output_unit,f_line) trim(op)//" skipped - invalid option"
+                    write(output_unit,f_line) ""
+                    i = i + 1
+            end select
+
+            ! all the switches were evaluated
+            if(i > command_argument_count()) exit
+        end do
+
+        ! check if all the mandatory options were selected
+        if(ui_filetype == ui_filetype_none) then
+            error_stop("input file(s) must be specified, see help -h")
+        end if
+
+        write(output_unit,f_line) heading(flat_pattern,"Evaluation of the program options - DONE")
+        write(output_unit,f_line) ""
+
+    end subroutine evaluate_program_options
+
+end module
--- lib/SFG_UTILS.f90
+++ lib/SFG_UTILS.f90
@@ -14,7 +14,11 @@ module SFG_UTILS
     use BOXDATA, only: boxdata_type
     implicit none
 
-    real(real64), parameter :: pi = acos(-1.0_real64)
+    real(real64), parameter ::  pi = acos(-1.0_real64),& ! -
+                                k_b = 1.386658d-23,&
+                                c = 2.99792458d10,& ! cm/s
+                                debye_to_ea = 0.208,& ! Debye to eA
+                                e_to_c = 1.6 ! 1e to C
 
     contains
 
@@ -38,5 +42,32 @@ module SFG_UTILS
         back_in_the_box_baby = modulo(back_in_the_box_baby , bd%box_dimensions) + bd%box_corner
 
     end function pbc_wrap
+
+!! cross_product(arg1, arg2)
+!! arg1, arg2 - 3d array vector
+!! returns cross product of arg1 and arg2
+!function cross_product(a,b)
+!	real*8, dimension(3) :: cross_product
+!	real*8, dimension(3), intent(IN) :: a, b
+!
+!	cross_product(1) = a(2) * b(3) - a(3) * b(2)
+!	cross_product(2) = a(3) * b(1) - a(1) * b(3)
+!	cross_product(3) = a(1) * b(2) - a(2) * b(1)
+!end function cross_product
+
+!subroutine fourier integral...
+
+!function s_to_HHHMMSS(seconds)
+!	character(14) :: s_to_HHHMMSS
+!	real*8, intent(IN) :: seconds
+!	integer :: hours, minutes, sec
+!	
+!	hours = seconds/3600
+!	minutes = (seconds-hours*3600)/60
+!	sec = seconds - hours*3600 - minutes*60
+!	
+!	write(s_to_HHHMMSS,'(I5.1,A1,I2.2,A1,I2.2)'), hours, ":", minutes, ":", sec
+!	s_to_HHHMMSS = adjustl(s_to_HHHMMSS)
+!end function s_to_HHHMMSS
 
 end module
