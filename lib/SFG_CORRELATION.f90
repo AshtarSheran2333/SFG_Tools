2c54fab9c15d8cad1bfc2ae79b8b9ea459649c80
AshtarSheran2333 <patrimusil@gmail.com>
2026-05-25 21:32:51 +0200

    add SFG_CORRELATION fill_history, skip_step

--- lib/SFG_CORRELATION.f90
+++ lib/SFG_CORRELATION.f90
@@ -15,13 +15,12 @@ module SFG_CORRELATION
         integer(int64), dimension(:), allocatable :: norm !(corrlen)
 
         integer(int64) ::	corrlen,& !number of samples based on boxdata $corrlen
-							t,& !time counter - used for the circular buffer
-                            samples
-        
+							t, nt1 !time counter - used for the circular buffer
         
     contains
 		procedure, public :: init
         procedure, public :: calculate_step
+        procedure, private :: fill_history
         !procedure, public :: skip_step
     end type
     
@@ -47,7 +46,6 @@ module SFG_CORRELATION
         !boxdata%DT (fs)
         this%corrlen = INT((1000 * boxdata%CORRLEN) / boxdata%DT) + 1
         this%t = 0 !nothing registered
-        this%samples = 0 !used to get the average
         
         !allocate everything...
         allocate(this%binder_history(struct_group%n_elements, this%corrlen))
@@ -72,41 +70,13 @@ module SFG_CORRELATION
         type(sfg_structure_group_type), intent(in) :: struct_group
         type(group_binder_type), intent(in) :: binder
         type(dXdrz_db_type), intent(in) :: dXdrz_db
-        type(boxdata_type), intent(in) :: boxdata
         integer(int8), dimension(:), allocatable :: layer_selection
+        type(boxdata_type), intent(in) :: boxdata
 
-        integer(int64) :: nt1, nt0, timelag, m, l
+        integer(int64) :: nt0, timelag, m
         real(real64) :: weight
-        real(real64), dimension(2) :: AM
         
-        nt1 = mod(this%t, this%corrlen) + 1 !ringbuffer index
-        this%t = this%t + 1
-        !clear binder
-        this%binder_history(:,nt1) = .false.
-        
-        !TODO make a function, OMP inside
-        !go over all SFG units
-        do m = 1, struct_group%n_elements
-            !get am of m
-            AM = struct_group%sfg_units(m)%get_AM(current_frame, dXdrz_db, boxdata)
-            !append a m
-            this%A_history(m, nt1) = AM(1)
-            this%M_history(m, nt1) = AM(2)
-            !update averages
-            this%avA(m) = this%avA(m) + (AM(1) - this%avA(m))/this%t 
-            this%avM(m) = this%avM(m) + (AM(2) - this%avM(m))/this%t 
-            !subtract the best averages currently available
-            this%A_history(m, nt1) = this%A_history(m,nt1) - this%avA(m)
-            this%M_history(m, nt1) = this%M_history(m,nt1) - this%avM(m)
-
-            !fill binder
-            do l = 1, size(layer_selection)
-                if(binder%layers(m) == layer_selection(l)) then
-                    this%binder_history(m,nt1) = .true.
-                    exit !this m was selected
-                end if
-            end do
-        end do
+        call this%fill_history(current_frame, struct_group, binder, dXdrz_db, layer_selection, boxdata)
 
         !TODO OMP
       	do timelag = 1, min(this%t,this%corrlen) !ramping the iterations from beginning...
@@ -117,9 +87,9 @@ module SFG_CORRELATION
             
 			do m = 1, struct_group%n_elements
 				! if the molecule is not in selected layer continue
-				if(.not. (this%binder_history(m,nt0) .or. this%binder_history(m, nt1))) cycle
+				if(.not. (this%binder_history(m,nt0) .or. this%binder_history(m, this%nt1))) cycle
 				
-				if(this%binder_history(m,nt0) .and. this%binder_history(m,nt1)) then
+				if(this%binder_history(m,nt0) .and. this%binder_history(m,this%nt1)) then
 					weight = 1.0_real64
 				else
 				! molecule M is present only in time nt1 or nt2
@@ -128,46 +98,66 @@ module SFG_CORRELATION
 				end if
                 
 				this%correlation_function(timelag) = this%correlation_function(timelag) &
-                        + (this%A_history(m, nt1)) * (this%M_history(m, nt0))
+                        + (this%A_history(m, this%nt1)) * (this%M_history(m, nt0))
 			end do
 		end do
-    
     end subroutine calculate_step
 
-!    subroutine skip_step(this)
-!        !fill binder
-!    
-!        !fill AM
-!    end subroutine skip_step
+    subroutine fill_history(this, current_frame, struct_group, binder, dXdrz_db, layer_selection, boxdata)
+        class(correlation_function_type), intent(inout) :: this
+        type(current_frame_type), intent(in) :: current_frame
+        type(sfg_structure_group_type), intent(in) :: struct_group
+        type(group_binder_type), intent(in) :: binder
+        type(dXdrz_db_type), intent(in) :: dXdrz_db
+        integer(int8), dimension(:), allocatable :: layer_selection
+        type(boxdata_type), intent(in) :: boxdata
+
+        integer(int64) :: m, l
+        real(real64), dimension(2) :: AM
+
+        this%nt1 = mod(this%t, this%corrlen) + 1 !ringbuffer index
+        this%t = this%t + 1
+        !clear binder
+        this%binder_history(:,this%nt1) = .false.
     
-	!binder in time(corrlen)
-    !A, M (SFG_UNITS, corrlen)
-    !avA, avM(SFG_UNITS)
-    
-	!if(mod(t,bd%self_skip) == 0) then 
-	!	!$OMP PARALLEL DEFAULT(SHARED)
-	!	!$OMP DO PRIVATE(timelag, nt0, m)
-	!	! self correlation function
-	!	do timelag = 0, min(t,bd%get_maxlag())
-	!		selfnorm(timelag) = selfnorm(timelag) + 1
-	!		nt0 = mod(t-timelag,corrlen)
-	!		do m = 1, bd%NO
-	!			! if the molecule is not in selected layer continue
-	!			if(.not. (binder_in_time(m,nt0) .or. binder_in_time(m, nt1))) cycle
-	!			
-	!			! molecule M is present in both times nt1 and nt0...
-	!			! full weight of the term
-	!			if(binder_in_time(m,nt0) .and. binder_in_time(m,nt1)) then
-	!				self_corr(timelag) = self_corr(timelag) + (Axx(m, nt1) - avAxx(m)) * (Mz(m, nt0)-avMz(m))
-	!			else
-	!			! molecule M is present only in time nt1 or nt2
-	!			! half weight for the term
-	!				self_corr(timelag) = self_corr(timelag) + 0.5d0 * (Axx(m, nt1) - avAxx(m)) * (Mz(m, nt0)-avMz(m))
-	!			end if
-	!		end do
-	!	end do
-	!	!$OMP END DO
-	!	!$OMP END PARALLEL
-	!end if
+        !TODO OMP
+        !go over all SFG units
+        do m = 1, struct_group%n_elements
+            !get am of m
+            AM = struct_group%sfg_units(m)%get_AM(current_frame, dXdrz_db, boxdata)
+            !append a m
+            this%A_history(m, this%nt1) = AM(1)
+            this%M_history(m, this%nt1) = AM(2)
+            !update averages
+            this%avA(m) = this%avA(m) + (AM(1) - this%avA(m))/this%t 
+            this%avM(m) = this%avM(m) + (AM(2) - this%avM(m))/this%t 
+            !subtract the best averages currently available
+            this%A_history(m, this%nt1) = this%A_history(m,this%nt1) - this%avA(m)
+            this%M_history(m, this%nt1) = this%M_history(m,this%nt1) - this%avM(m)
+
+            !fill binder
+            do l = 1, size(layer_selection)
+                if(binder%layers(m) == layer_selection(l)) then
+                    this%binder_history(m,this%nt1) = .true.
+                    exit !this m was selected
+                end if
+            end do
+        end do
+    end subroutine fill_history
+
+    subroutine skip_step(this, current_frame, struct_group, binder, dXdrz_db, layer_selection, boxdata)
+        class(correlation_function_type), intent(inout) :: this
+        type(current_frame_type), intent(in) :: current_frame
+        type(sfg_structure_group_type), intent(in) :: struct_group
+        type(group_binder_type), intent(in) :: binder
+        type(dXdrz_db_type), intent(in) :: dXdrz_db
+        integer(int8), dimension(:), allocatable :: layer_selection
+        type(boxdata_type), intent(in) :: boxdata
         
+        !now just call fill_history
+        call this%fill_history(current_frame, struct_group, binder, dXdrz_db, layer_selection, boxdata)
+
+    end subroutine skip_step
+        
+    
 end module SFG_CORRELATION
